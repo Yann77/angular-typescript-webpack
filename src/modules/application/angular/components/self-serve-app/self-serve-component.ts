24ac40e91f6aaa26753d9b63e428655709992775
jmdagenais <jmdagenais@acquisio.com>
2016-05-12 11:00:49 -0400

    use multiline string in template

--- src/modules/application/angular/components/self-serve-app/self-serve-component.ts
+++ src/modules/application/angular/components/self-serve-app/self-serve-component.ts
@@ -2,22 +2,22 @@ import Component from '../../../../../decorators';
 import "angular-material/angular-material.min.css";
 
 @Component('app.application','selfServeComponent', {
-    template: '<md-content flex="100">' +
-    '<div>' +
-    '<md-card>' +
-    '<md-toolbar>' +
-    '<div class="md-toolbar-tools">' +
-        '<h2>' +
-            '<md-icon md-svg-icon="md-menu"></md-icon><span>Steps</span>' +
-        '</h2>' +
-    '</div>' +
-    '</md-toolbar>' +
-    '<md-stepper-card-content class="md-no-padding">' +
-        'STEPPER CONTENT' +
-    '</md-stepper-card-content>' +
-    '</md-card>' +
-    '</div>' +
-    '</md-content>'
+    template: `<md-content flex="100">
+    <div>
+    <md-card>
+    <md-toolbar>
+    <div class="md-toolbar-tools">
+        <h2>
+            <md-icon md-svg-icon="md-menu"></md-icon><span>Steps</span>
+        </h2>
+    </div>
+    </md-toolbar>
+    <md-stepper-card-content class="md-no-padding">
+        STEPPER CONTENT
+    </md-stepper-card-content>
+    </md-card>
+    </div>
+    </md-content>`
     //$routeConfig: [
     //    {path: '/signupConfirmation', name: 'SignupConfirmationStep', component: 'signupConfirmationComponent', useAsDefault: true},
     //    {path: '/provider', name: 'ProviderStep', component: 'providerComponent'},
