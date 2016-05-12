d0e02ff8f561066b9c8eca5a4bfe44a74d59cefd
Yann77 <yann0795@gmail.com>
2016-05-12 00:17:53 -0400

    Trying to find steps....

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,17 @@ import "@angular/router/angular1/angular_1_router";
 
 // load our default (non specific) css
 import "font-awesome/css/font-awesome.css";
-import "bootstrap/dist/css/bootstrap.css";
+import "angular-material/angular-material.min.css";
 import "./styles/screen.scss";
 
-import "./modules/application/angular/components/selfServeApplication/SelfServeComponent";
+// load specific components
+import "./modules/application/angular/components/self-serve-app/self-serve-component";
 
-//angular.module("app", ["ngComponentRouter", "app.application", "app.tweets", "app.about"])
 angular.module('app', ['ngComponentRouter', 'app.application'])
     .value('$routerRootComponent', 'app')
     .component('app', {
         template:
-        '<h1>Self Serve Signup process</h1>\n'+
+        '<h1>Self Serve Signup Process</h1>\n'+
         '<ng-outlet></ng-outlet>\n',
         $routeConfig: [
             {path: '/', name: 'SelfServeComponent', component: 'selfServeComponent', useAsDefault: true}
--- src/modules/application/angular/components/self-serve-app/self-serve-component.ts
+++ src/modules/application/angular/components/self-serve-app/self-serve-component.ts
@@ -1,7 +1,23 @@
 import Component from '../../../../../decorators';
+import "angular-material/angular-material.min.css";
 
 @Component('app.application','selfServeComponent', {
-    template: '<h2>Steps</h2>'
+    template: '<md-content flex="nogrow" style="width: 800px; margin: 40px auto">' +
+    '<div>' +
+    '<md-card>' +
+    '<md-toolbar>' +
+    '<div class="md-toolbar-tools">' +
+        '<h2>' +
+            '<md-icon md-svg-icon="md-menu"></md-icon><span>Steps</span>' +
+        '</h2>' +
+    '</div>' +
+    '</md-toolbar>' +
+    '<md-stepper-card-content class="md-no-padding">' +
+        'STEPPER CONTENT' +
+    '</md-stepper-card-content>' +
+    '</md-card>' +
+    '</div>' +
+    '</md-content>'
     //$routeConfig: [
     //    {path: '/signupConfirmation', name: 'SignupConfirmationStep', component: 'signupConfirmationComponent', useAsDefault: true},
     //    {path: '/provider', name: 'ProviderStep', component: 'providerComponent'},
